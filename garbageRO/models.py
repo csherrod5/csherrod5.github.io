99a69b990ded21e6a9e362dbb495e0370d57f80a
csherrod5 <45182890+csherrod5@users.noreply.github.com>
2018-11-19 21:53:16 -0600

    Add files via upload

--- garbageRO/models.py
+++ garbageRO/models.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 @login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(int(user_id))
-	
+
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	password = db.Column(db.String(60), nullable=False)
@@ -18,15 +18,15 @@ class Dumpster(db.Model):
 	lat = db.Column(db.Float, nullable=False)
 	lng = db.Column(db.Float, nullable=False)
 	pickups = db.relationship('Pickup', backref='dumpster', lazy=True)
-	
+
 	def __repr__(self):
-		return f"Dumpster('{self.id}', '{self.location}')"
-	
+		return "Dumpster({}, {})".format(self.id, self.location)
+
 class Pickup(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 	time = db.Column(db.String, nullable=True, default='midnight')
 	dumpsterLocation = db.Column(db.String, db.ForeignKey('dumpster.location'), nullable=False)
-	
+
 	def __repr__(self):
-		return f"Pickup('{self.id}', 'dumpster.location', '{self.date}')"
+		return "Pickup({}, {}, {})".format(self.id, dumpster.location, self.date)
